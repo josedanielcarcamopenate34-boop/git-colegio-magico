3e337bed4201b402d1f5697e27720c64cddc05db
josedanielcarcamopenate34-boop <josedanielcarcamopenate34@gmail.com>
2026-09-05 16:06:11 +0000

    feat: reto 2 solucionado

--- src/main/java/com/colegiomagico/reto2/Aritmancia.java
+++ src/main/java/com/colegiomagico/reto2/Aritmancia.java
@@ -5,8 +5,8 @@ package com.colegiomagico.reto2;
  */
 public class Aritmancia {
 
-    public static int sumar(int a, int b) {
-        return 0; // TODO: Cambia esto
+    public static int sumar(int numero1, int numero2) {
+        return numero1 + numero2; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
